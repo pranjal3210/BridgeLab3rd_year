5ec516880e2c49d71e548f55fa2da2871c5076a0
Pranjal Pandey <pranjal3210@gmail.com>
2025-09-08 21:23:05 +0530

    Keep only class and object folder, remove others

--- programming elements/level-1/samAveragePCM.java
+++
@@ -1,9 +0,0 @@
-public class samAveragePCM {
-    public static void main(String[] args) {
-        int mathsMark = 94;
-        int physicsMark = 95;
-        int chemistryMark = 96;
-        double averageMarks = (mathsMark + physicsMark + chemistryMark) / 3.0;
-        System.out.println("Sam’s average mark in PCM is " + averageMarks);
-    }
-}
